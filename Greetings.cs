a953d67cc5f103f2e68fc37c0e67691a74038c1e
Annal3sa <annalesapelky12905@gmail.com>
2023-04-14 14:08:32 -0500

    Initial commit

---
+++ Greetings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pelky_Annalesa_Classes
+{
+    class Greetings
+    {
+        /// <summary>
+        ///  
+        /// </summary>
+ 
+        public void Welcome()
+        {
+            Console.WriteLine("Hello Traveler, welcome!");
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        public void Hello(string name)
+        {
+            Console.WriteLine($"{name}, thank you for joining us today!");
+        }
+
+        
+
+    }
+}
